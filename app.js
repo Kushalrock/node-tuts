bfff882e915530d1c42be917729dce99d7214266
Kushal <agrawalskushal@gmail.com>
2021-02-16 20:20:07 +0530

    Made the pug website...

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const port= 10000;
 const app = express();
 
+app.use('/static',express.static('public'));
 // PUG CONFIGURATION
 app.set('view engine','pug');
 app.set('views',path.join(__dirname,'views'));
